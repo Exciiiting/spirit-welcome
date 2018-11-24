641dad1b1ef5f2961d674da7701c0ba2b58f4e11
Jnaby <44756627+kwras@users.noreply.github.com>
2018-11-24 15:26:52 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ codes.send( " *Welcome to* **Spirrrit.** ");
  client.on('ready',async () => {
 console.log("Starting..");
 let g = client.guilds.get("507500031994101763");
-let c = g.channels.get("507519447028727829");
+let c = g.channels.get("507854226769051659");
 if(c.type === 'voice') {
 c.join();
 setInterval(() => {

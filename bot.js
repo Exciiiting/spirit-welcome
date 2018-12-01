ff16cb7935e5d291a14d92b1eab852ce94146324
Jnaby <44756627+kwras@users.noreply.github.com>
2018-12-01 13:19:56 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ client.on('guildMemberAdd',async member => {
   if(member.guild.id !== '507500031994101763') return;
   setTimeout(function(){
   member.guild.channels.find(r => r.id === '518188696223088665').send(' *Welcome to* **Spirit.**');
-},3000);
+},6000);
 });
 
 
